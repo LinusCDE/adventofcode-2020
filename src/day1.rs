02101818ff4c05733735cd346294b1c64d12a9a8
Linus <linus@cosmos-ink.net>
2020-12-09 19:47:44 +0100

    Solve day 1

---
+++ src/day1.rs
@@ -0,0 +1,48 @@
+use anyhow::Result;
+
+#[aoc_generator(day1)]
+pub fn parse_input(input: &str) -> Vec<u16> {
+    input
+        .split("\n")
+        .map(|line| line.parse().unwrap())
+        .collect()
+}
+
+#[aoc(day1, part1)]
+pub fn solve_part1(input: &Vec<u16>) -> Result<u32> {
+    for i in 0..input.len() {
+        for j in 0..input.len() {
+            if i == j {
+                continue;
+            }
+            let i = input[i];
+            let j = input[j];
+            if i + j == 2020 {
+                return Ok(i as u32 * j as u32);
+            }
+        }
+    }
+
+    bail!("No number found!");
+}
+
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &Vec<u16>) -> Result<u64> {
+    for i in 0..input.len() {
+        for j in 0..input.len() {
+            for k in 0..input.len() {
+                if i == j || j == k || i == k {
+                    continue;
+                }
+                let i = input[i];
+                let j = input[j];
+                let k = input[k];
+                if i + j + k == 2020 {
+                    return Ok(i as u64 * j as u64 * k as u64);
+                }
+            }
+        }
+    }
+
+    bail!("No number found!");
+}
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#[macro_use]
+extern crate aoc_runner_derive;
+#[macro_use]
+extern crate anyhow;
+
+mod day1;
+
+aoc_lib! { year = 2020 }
