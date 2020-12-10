eb88cb7c5087e50f771109c1962b1ebc487bc72a
Linus <linus@cosmos-ink.net>
2020-12-10 09:09:42 +0100

    Solve day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use regex::Regex;
 
 pub struct Map {
@@ -39,6 +40,23 @@ impl Map {
     pub fn height(&self) -> usize {
         self.data.len()
     }
+
+    pub fn count_trees(&self, slope_vector: (usize, usize)) -> Result<usize> {
+        ensure!(slope_vector.1 > 0, "This slope will actually end");
+        let mut tree_count = 0;
+        let mut x = 0;
+        let mut y = 0;
+        while y < self.height() {
+            if self.is_tree(x, y) {
+                tree_count += 1;
+            }
+
+            x += slope_vector.0;
+            y += slope_vector.1;
+        }
+
+        Ok(tree_count)
+    }
 }
 
 #[aoc_generator(day3)]
@@ -47,19 +65,16 @@ pub fn parse_input(input: &str) -> Map {
 }
 
 #[aoc(day3, part1)]
-pub fn solve_part1(input: &Map) -> usize {
-    let mut tree_count = 0;
-    let mut x = 0;
-    for y in 0..input.height() {
-        if input.is_tree(x, y) {
-            tree_count += 1;
-        }
-
-        x += 3;
-    }
-
-    tree_count
+pub fn solve_part1(input: &Map) -> Result<usize> {
+    input.count_trees((3, 1))
 }
 
-//#[aoc(day3, part2)]
-//pub fn solve_part2(input: &Map) -> u16 {}
+#[aoc(day3, part2)]
+pub fn solve_part2(input: &Map) -> Result<usize> {
+    let mut result = 1;
+    for slope_vector in &[(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)] {
+        result *= input.count_trees(*slope_vector)?;
+    }
+
+    Ok(result)
+}
