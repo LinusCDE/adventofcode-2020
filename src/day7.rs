ec79b7aac6e8519541d2f85f2618226e8d9dc059
Linus <linus@cosmos-ink.net>
2020-12-13 20:38:51 +0100

    Solve day 7 part 1

---
+++ src/day7.rs
@@ -0,0 +1,101 @@
+use anyhow::Result;
+use std::collections::{HashMap, HashSet};
+
+type Bag = u16;
+
+#[derive(Debug, Default)]
+pub struct Bags {
+    bag_id_counter: u16,
+    bag_ids: HashMap<String, Bag>,
+    containments: HashMap<Bag, HashSet<(Bag, u8)>>,
+}
+
+impl Bags {
+    pub fn get_bag(&self, color: &str) -> Option<Bag> {
+        self.bag_ids.get(color).map(|bag| *bag)
+    }
+
+    pub fn get_color(&self, bag: Bag) -> Option<String> {
+        self.bag_ids.iter().find(|(_, id)| **id == bag).map(|(color, _)| color.to_owned())
+    }
+
+    pub fn get_or_create_bag(&mut self, color: &str) -> Bag {
+        if let Some(bag) = self.get_bag(color) {
+            return bag;
+        }
+        let bag = self.bag_id_counter;
+        self.bag_id_counter += 1;
+        self.bag_ids.insert(color.to_owned(), bag);
+        return bag;
+    }
+
+    pub fn get_bag_containments(&self, bag: Bag) -> Option<&HashSet<(Bag, u8)>> {
+        self.containments.get(&bag)
+    }
+
+    pub fn add_bag_containment(&mut self, bag: Bag, another_bag: Bag, amount: u8) -> Result<()> {
+        let bag_containments = self.containments.entry(bag).or_default();
+        if bag_containments.iter().any(|(bag, amount)| *bag == another_bag) {
+            bail!("This bag was already added!")
+        }
+        bag_containments.insert((another_bag, amount));
+        Ok(())
+    }
+
+    pub fn bags(&self) -> impl Iterator<Item = &Bag> {
+        self.bag_ids.iter().map(|(color, id)| id)
+    }
+
+    pub fn contains_bag_recursively(&self, bag: Bag, searched_bag: Bag) -> bool {
+        if let Some(containments) = self.get_bag_containments(bag) {
+            for (contained, _) in containments {
+                if *contained == searched_bag || self.contains_bag_recursively(*contained, searched_bag) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
+
+#[aoc_generator(day7)]
+pub fn parse_input(input: &str) -> Result<Bags> {
+    let mut bags = Bags::default();
+    // Find all bags
+    for line in input.split('\n') {
+        let words: Vec<&str> = line.split(' ').collect();
+        let color = format!("{} {}", words[0], words[1]);
+        ensure!(words[2] == "bags", "Colors are expected to be always 2 words");
+        let bag = bags.get_or_create_bag(&color);
+        // Add bags
+
+        for contained_bag in line[line.find("contain ").ok_or(anyhow!("Invalid line"))? + "contain ".len()..line.len() - 1].split(", ") {
+            if contained_bag == "no other bags" {
+                continue;
+            }
+
+            let another_bag_split: Vec<&str> = contained_bag.split(' ').collect();
+            ensure!(another_bag_split.len() == 4, "Expecting 4 words: NUM COLOR COLOR bag(s)");
+            let another_color = format!("{} {}", another_bag_split[1], another_bag_split[2]);
+            let another_amount = another_bag_split[0].parse()?;
+            let another_bag = bags.get_or_create_bag(&another_color);
+
+            bags.add_bag_containment(bag, another_bag, another_amount)?;
+        }
+    }
+
+    Ok(bags)
+}
+
+#[aoc(day7, part1)]
+pub fn solve_part1(input: &Bags) -> Result<usize> {
+    //input.bags().for_each(|bag| println!("Bag: {:?}", input.get_color(*bag)));
+    let searched_bag = input.get_bag("shiny gold").ok_or(anyhow!("Shiny gold bag not found"))?;
+    Ok(input.bags().filter(|bag| input.contains_bag_recursively(**bag, searched_bag)).count())
+}
+
+#[aoc(day7, part2)]
+pub fn solve_part2(input: &Bags) -> usize {
+    todo!()
+}
